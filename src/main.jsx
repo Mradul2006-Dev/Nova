15ecd0fde15a62198907be0ee4d02de1d3d90450
Mradul Sharma <mradulsharma272006@gmail.com>
2026-06-30 02:55:55 +0530

    fix stairs import

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Stairs from './components/common/stairs.jsx'
+import Stairs from './components/common/Stairs.jsx'
 import NavbarContext from "./components/Context/NavbarContext.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
